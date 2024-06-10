628ef985336558b1fa859d438d092f35ae3bf2ea
qiosion <qiosion@gmail.com>
2024-06-10 13:50:52 +0900

    input, v-model

--- src/App_10_emit.vue
+++ src/App_10_emit.vue
@@ -5,8 +5,25 @@
 
   <Discount v-bind="Object" />
 
+<!-- 
+  props 쓰는 법
+    1. 부모 컴포넌트에서 v-bind: 또는 : 를 사용하여 보내고싶은 데이터 전송 
+        :변수명="전송하고자 하는 데이터(아래에서 설정한 값)"
+        colon 은 속성에 데이터 바인딩할 때, props 전송할 때 사용
+    2. 자식 컴포넌트에서 default 값에 props 등록
+        json 형태로 등록, {데이터 이름: 자료형 이름}
+    
+  $emit() 함수
+    props 는 read-only. 자식 컴포넌트에서 수정하면 에러 발생.
+    이때 custom event 로 자식이 메시지 전송하여 해결
+      자식 -> 부모 메시지 전송 :$emit('변수명') 또는  $emit('변수명', 데이터)
+      부모가 메시지 수신 : @변수명="해당 변수명을 수신했을 때 실행할 js 코드"
+                        이때 js 코드 안에서 $event 를 사용하여 받은 데이터를 사용 가능
+    $emit() 을 함수 안에 쓰고싶다면 this.$emit()
+-->
+
   <Modal :onerooms="onerooms" :clicked="clicked" :isModalOpen="isModalOpen"
-  @closeModal="isModalOpen = false"/>
+  @closeModal="isModalOpen = false; clicked = $event"/>
 
   <Card @openModal="isModalOpen = true; clicked = $event"
   v-for="(oneroom, i) in onerooms"
@@ -39,7 +56,8 @@ export default {
     
   },
   components: {
-    Discount: Discount,
+    // Discount: Discount, // 키와 값이 같으면
+    Discount, // 하나로 축약 가능함
     Modal,
     Card,
   },
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,9 +3,24 @@
     <div class="white-bg">
       <h4>{{ onerooms[clicked].title }}</h4>
       <img class="room-img" :src="onerooms[clicked].image">
-      <p>{{ onerooms[clicked].price }} 원</p>
       <p>{{ onerooms[clicked].content }}</p>
-      <button @click="closeFunction">닫기</button>
+<!--
+    $event : 이벤트리스너
+        $event.target : 현재 이벤트가 발생하고 있는 요소(element)
+        <input @input="month = $event.target.value" type="number">
+    v-model : 이벤트 리스너로 js 코드작성하면 너무 길어. 축약함.
+        여기 작성한 값을 아래 data에 지정한 변수에 넣어줌
+    
+    input : 사용자가 숫자를 입력해도 전무 문자 자료형으로 저장됨
+        <input v-model="month"> 개월
+    
+-->
+      <select v-model="month" style="width: 50px;">
+        <option>12</option>
+        <option>24</option>
+      </select> 개월
+      <p>{{ month }} 개월 당 {{ onerooms[clicked].price * month }} 원</p>
+      <button @click="$emit('closeModal')">닫기</button>
     </div>
   </div>
 </template>
@@ -13,15 +28,16 @@
 <script>
 export default {
   name: 'Modal',
+  data() {
+    return {
+      month: 1,
+    }
+  },
   props: {
     // 가져온 데이터 명: 타입(틀려도 됨)
     onerooms: Array,
     clicked: Number,
     isModalOpen: Boolean,
-  }, methods: {
-    closeFunction() {
-      this.$emit('closeModal', this.clicked)
-    }
   }
 }
 </script>
