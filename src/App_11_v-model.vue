8ea13aa6190afd897dde6979d782e36cc761ceb3
qiosion <qiosion@gmail.com>
2024-06-10 14:12:42 +0900

    watcher

---
+++ src/App_11_v-model.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="menu">
+    <a v-for="i in menu" :key="i">{{ i }}</a>
+  </div>
+
+  <Discount v-bind="Object" />
+
+  <Modal :onerooms="onerooms" :clicked="clicked" :isModalOpen="isModalOpen"
+  @closeModal="isModalOpen = false"/>
+
+  <Card @openModal="isModalOpen = true; clicked = $event"
+  v-for="(oneroom, i) in onerooms"
+  :key="i"
+  :oneroom="onerooms[i]" />
+
+</template>
+
+<script>
+import data from './assets/oneroom.js';
+import Discount from './components/Discount.vue';
+import Modal from './components/Modal.vue';
+import Card from './components/Card.vue';
+
+export default {
+  name: "App",
+  data() {
+    return {
+      Object: {
+        name: 'kim',
+        age: 20,
+      },
+      clicked: 0, // 클릭한 제목
+      isModalOpen: false, // 1. UI 의 현재 상태를 데이터로 저장
+      menu: ['Home', 'Products', 'About'],
+      onerooms: data,
+    };
+  },
+  methods: { // 함수
+    
+  },
+  components: {
+    Discount: Discount,
+    Modal,
+    Card,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.discount {
+  background: #eee;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+.menu {
+  background-color: rgb(42, 56, 117);
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -11,14 +11,12 @@
     v-model : 이벤트 리스너로 js 코드작성하면 너무 길어. 축약함.
         여기 작성한 값을 아래 data에 지정한 변수에 넣어줌
     
-    input : 사용자가 숫자를 입력해도 전무 문자 자료형으로 저장됨
-        <input v-model="month"> 개월
-    
+    input : 사용자가 숫자를 입력해도 문자 자료형으로 저장됨
+
+    watcher : data 감시하는 함수. 사용자의 입력을 받는 부분(input 요소)에서 꼭 필요함!
+        숫자만 입력하게 하고싶은데 문자를 입력하면 경고
 -->
-      <select v-model="month" style="width: 50px;">
-        <option>12</option>
-        <option>24</option>
-      </select> 개월
+      <input v-model="month" style="width: 40px;"> 개월
       <p>{{ month }} 개월 당 {{ onerooms[clicked].price * month }} 원</p>
       <button @click="$emit('closeModal')">닫기</button>
     </div>
@@ -31,6 +29,15 @@ export default {
   data() {
     return {
       month: 1,
+    }
+  },
+  watch: { // 데이터 감시. 데이터가 변할 때 마다 watch에 있는 코드가 실행됨
+    // 감시할 데이터 변수명을 함수형태로 작성
+    month(a) { // month (변경 후(변경 될) month 데이터, 변경 전 month 데이터) 로 파라미터 설정 가능
+      if (a > 24 || a < 1) {
+        alert("최소 1개월에서 최대 24개월까지 계약 가능");
+        this.month = 1;
+      }
     }
   },
   props: {
